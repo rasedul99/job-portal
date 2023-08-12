13d96ebe6bcb7447a528bd1f0f819b0a0e9fe816
MD Rasedul Islam <rasedul2199@gmail.com>
2023-08-13 01:08:49 +0600

    crud operation done

--- server/index.js
+++ server/index.js
@@ -39,6 +39,22 @@ async function run() {
     const db = client.db("jobPortal");
     const collection = db.collection("jobPosts");
 
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
+
+    // create a document to be inserted
+    app.post("/createPost", async (req, res) => {
+      const post = req.body;
+      const result = await collection.insertOne(post);
+      res.send(result);
+    });
+
     // get all posts
     app.get("/posts", verifyJWT, async (req, res) => {
       const cursor = collection.find({});
@@ -52,6 +68,36 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const post = await collection.findOne(query);
       res.send(post);
+    });
+
+    // delete a single post
+    app.delete("/deletePost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await collection.deleteOne(query);
+      res.send(result);
+    });
+
+    // update a single post
+    app.put("/updatePost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          postName: req.body.postName,
+          companyName: req.body.companyName,
+          numberOfVacancy: req.body.numberOfVacancy,
+          currentDate: req.body.currentDate,
+          lastApplyDate: req.body.lastApplyDate,
+          jobDescription: req.body.jobDescription,
+          jobRequirements: req.body.jobRequirements,
+          jobBenefits: req.body.jobBenefits,
+          howToApply: req.body.howToApply,
+        },
+      };
+      const result = await collection.updateOne(query, updateDoc, options);
+      res.send(result);
     });
   } finally {
   }
