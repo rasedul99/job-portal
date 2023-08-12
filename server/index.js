881308bf4538f8558cf4cbfbe068dfbd1087f14d
MD Rasedul Islam <rasedul2199@gmail.com>
2023-08-13 00:17:11 +0600

    connect with mongodb

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv:// ${process.env.DB_USER}:${process.env.DB_PASS}@jobportal.ydluey6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+
 app.get("/", (req, res) => {
   res.send("Job Portal Server Running!");
 });
